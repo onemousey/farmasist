9fef25529126c70f64ef757d35c6dc74338f6565
onemousey <ionut.brinzei@gmail.com>
2018-04-03 23:17:02 +0300

    Adaugat Proiect de la Antonia

---
+++ Farmasist2/Scripts/JavaScript.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+
+    var $active = true;
+
+    $('.panel-title > a').click(function (e) {
+        e.preventDefault();
+    });
+
+    $('.collapse-init').on('click', function () {
+        if (!$active) {
+            $active = true;
+            $('.panel-title > a').attr('data-toggle', 'collapse');
+            $('.panel-collapse').collapse({ 'toggle': true, 'parent': '#accordion' });
+            $(this).html('Click to disable accordion behavior');
+        } else {
+            $active = false;
+            $('.panel-collapse').collapse({ 'toggle': true, 'parent': '#accordion' });
+            $('.panel-title > a').removeAttr('data-toggle');
+            $(this).html('Click to enable accordion behavior');
+        }
+    });
+
+});
