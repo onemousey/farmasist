628e68747e29f6d2a707b984102ede721b7eca8c
DenisaSzucs <denisa.szucs@gmail.com>
2018-05-05 15:48:59 +0300

    adaugare buc. la reteta

--- Farmasist2/Models/Reteta.cs
+++ Farmasist2/Models/Reteta.cs
@@ -10,9 +10,14 @@ namespace Farmasist2.Models
         public string Nume_pacient { get; set; }
         public string Diagnostic { get; set; }
         public string Medicament1 { get; set; }
+        public int Cant1 { get; set; }
         public string Medicament2 { get; set; }
+        public int Cant2 { get; set; }
         public string Medicament3 { get; set; }
+        public int Cant3 { get; set; }
         public string Medicament4 { get; set; }
+        public int Cant4 { get; set; }
         public string Medicament5 { get; set; }
+        public int Cant5 { get; set; }
     }
 }
